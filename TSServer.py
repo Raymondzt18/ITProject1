25c9d135b12cffe34054efea67a82c6aa5dd91c1
Raymond Tan <rt503@null.cs.rutgers.edu>
2018-10-02 16:30:25 -0400

    First commit

---
+++ TSServer.py
@@ -0,0 +1,72 @@
+
+import numpy as mypy
+import threading
+import time
+import random
+import pprint
+
+import socket as mysoc
+
+def removeChar(c, l): #Given a character, removes all occurrences from list
+    result = []
+    for element in l:
+        if element != c:
+            result.append(element)
+    return result
+
+
+def server():
+    name_of_file = "PROJI-DNSTS.txt"
+    f = open(name_of_file, 'r')
+
+    DNS_table = {}
+
+    for line in f:
+    
+        triplet = line.split()
+
+        if triplet:
+            DNS_table[triplet[0]]=((triplet[1], triplet[2]))
+    pprint.pprint(DNS_table) 
+
+    try:
+        socket = mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
+        print("[Server]: Server socket created")
+    except mysoc.error as err:
+        print('{} \n'.format("socket open error ",err))
+
+    server_binding = ('', 60001)
+    socket.bind(server_binding)
+    socket.listen(1) #parameter  is the maximum number of queued connections aka backlog
+    host = mysoc.gethostname()
+    print("[Server]: Server host name is: ",host)
+    localhost_ip = (mysoc.gethostbyname(host))
+    print("[Server]: Server IP address is ",localhost_ip)
+
+    conn_id, addr = socket.accept()
+    print("[Server]: Got a connection request from client at ",addr)
+    while True:
+        msg = conn_id.recv(100)
+        msg = msg.decode('utf-8')
+        if not msg:
+            print("Client disconnected")
+            break
+        
+        #Do DNS table lookup
+        split_msg = msg.split()
+
+        record = DNS_table.get(split_msg[0])
+        return_msg = ''
+
+        if record:
+            return_msg = split_msg[0] + record[0] + record[1]
+            return_msg = return_msg.encode('utf-8')
+        else:
+            return_msg = "Hostname - Error:HOST NOT FOUND".encode('utf-8')
+
+        conn_id.send(return_msg)
+
+    socket.close()
+    exit()
+    
+server()
